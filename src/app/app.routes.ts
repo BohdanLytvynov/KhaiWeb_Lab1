33b82a36fb02d42ba8604b667903614102aaf5c6
BohdanLytvynov <blitvinov19@gmail.com>
2024-09-16 02:16:53 +0300

    FeedBack form added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './Components/start-page/start-page.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: StartPageComponent},
+    {path: "feedback", component: FeedbackComponent},
+    {path: "**", redirectTo: "/"}
+];
