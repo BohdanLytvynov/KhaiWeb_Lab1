9d22c7e29a3439aac6fc10220f14af7029ad8fe4
BohdanLytvynov <blitvinov19@gmail.com>
2024-09-16 03:19:17 +0300

    Sign Up added

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login : string = '';
+  pass : string = '';
 
+  //Functions
+
+  onLoginPressed()
+  {
+    
+  }
 }
--- src/app/Components/sign-up/sign-up.component.ts
+++ src/app/Components/sign-up/sign-up.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+   login : string = '';
+   firstname : string = '';
+   lastname : string = '';
+   pass1 : string = '';
+   pass2 : string = '';
+   agree : boolean = false;
 
+   //Functions
+
+   onPassChanged(value : string)
+   {
+      
+   }
+
+   onSignIn()
+   {
+
+   }
 }
